482526d2add4fea7c3fbefa9b3cc87cbb732f377
Rameshwar Singh <rameshwar.job2@gmail.com>
2023-12-28 13:40:36 -0500

    RNN Compile

---
+++ rnn.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:,1:2].values
+
+sc = MinMaxScaler(feature_range= (0,1))
+training_set_scalaed = sc.fit_transform(training_set)
+
+x_train = []
+y_train = []
+
+for i in range(60,1258):
+  x_train.append(training_set_scalaed[i-60:i,0])
+  y_train.append(training_set_scalaed[i,0])
+
+x_train,y_train = np.array(x_train), np.array(y_train)
+
+x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+
+regressor = Sequential()
+regressor.add(LSTM(units = 50, return_sequences= True, input_shape = (x_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences= True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences= True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units = 1))
+
+#compiling the RNN code
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
